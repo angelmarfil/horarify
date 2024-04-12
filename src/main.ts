b832666a79a262b8a403c478c2560f27789827da
Angel Marfil <angelmarfil11@gmail.com>
2024-04-12 17:46:44 -0500

    configurando toasts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,33 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast, { POSITION, type PluginOptions } from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const options: PluginOptions = {
+  position: POSITION.TOP_RIGHT,
+  timeout: 5000,
+  transition: 'Vue-Toastification__fade',
+  maxToasts: 2,
+  closeOnClick: false,
+  pauseOnFocusLoss: false,
+  pauseOnHover: false,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+  rtl: false
+}
+
 app.use(createPinia())
 app.use(router)
+app.use(Toast, options)
 
 app.mount('#app')
