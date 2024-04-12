a7565747ed8a86ea203d3f05e37e99fe20331c56
Gabriel Caamal <Carlos.chancaamal@gmail.com>
2024-04-12 07:55:58 -0500

    agregando vistas principales y modificando router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TimetableView from '@/views/TimetableView.vue'
+import TasksView from '@/views/TasksView.vue'
+import ConfigView from '@/views/ConfigView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'timetable',
+      component: TimetableView
+    },
+    {
+      path: '/tasks',
+      name: 'tasks',
+      component: TasksView
+    },
+    {
+      path: '/config',
+      name: 'config',
+      component: ConfigView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
   ]
--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
-
 <template>
-  <main>
+  <div>
+    <h1>Hola mundo</h1>
     <button class="btn">Button</button>
     <button class="btn btn-neutral">Neutral</button>
     <button class="btn btn-primary">Primary</button>
@@ -9,5 +9,7 @@
     <button class="btn btn-accent">Accent</button>
     <button class="btn btn-ghost">Ghost</button>
     <button class="btn btn-link">Link</button>
-  </main>
+  </div>
 </template>
+
+<style scoped></style>
