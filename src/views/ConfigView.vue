1c0d1d7b869b386b5f2edd74e44ae1030cba4dfc
Alfonso Estrada <21393135@utcancun.edu.mx>
2024-04-13 13:16:28 -0500

    Agregando vista de configuración y opción para cerrar sesión

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -1,6 +1,103 @@
-<script setup lang="ts"></script>
-<template>
-    <div></div>
-</template>
+<script setup lang="ts">
+import router from '@/router'
+import { useUserStore } from '@/stores/user'
+import { useToast } from 'vue-toastification'
 
+const toast = useToast()
+
+const userStore = useUserStore()
+
+function logout() {
+  userStore.logout()
+  toast.info('Se ha cerrado la sesión', { timeout: 2000 })
+  router.push({ name: 'login' })
+}
+</script>
+<template>
+  <div class="m-4">
+    <ul class="menu bg-base-200 rounded-box w-full">
+      <li>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+          Item 2
+        </a>
+      </li>
+      <li>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          Item 1
+        </a>
+      </li>
+      <li>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+            />
+          </svg>
+          Item 3
+        </a>
+      </li>
+    </ul>
+
+    <ul class="menu bg-base-200 rounded-box w-full mt-4">
+      <li>
+        <a @click="logout"
+          >Cerrar Sesión
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5 absolute right-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+            />
+          </svg>
+        </a>
+      </li>
+    </ul>
+
+    <p class="w-full text-center fixed bottom-20 left-0">Versión 1.0 Alpha</p>
+  </div>
+</template>
 <style scoped></style>
