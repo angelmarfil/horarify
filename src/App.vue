dd71dd1bc60fc91d83e14a5f2e63d24423a079e3
Airan Campos <21393136@utcancun.edu.mx>
2024-04-13 11:49:19 -0500

    ocultar layouts cuando el usuario este en la vista de login o registro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import Navbar from '@/layout/NavbarLayout.vue'
 import BtmNav from '@/layout/BtmNavLayout.vue'
+const route = useRoute()
 </script>
 
 <template>
-  <Navbar></Navbar>
+  <Navbar v-if="route.name !== 'login' && route.name !== 'register'"></Navbar>
 
   <main>
     <RouterView />
   </main>
 
-  <BtmNav></BtmNav>
+  <BtmNav v-if="route.name !== 'login' && route.name !== 'register'"></BtmNav>
 </template>
 
 <style scoped></style>
