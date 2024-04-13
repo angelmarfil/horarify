0f7b6decb00de998e969abeea03f784833b382a5
Gabriel Caamal <Carlos.chancaamal@gmail.com>
2024-04-13 11:29:15 -0500

    añadiendo seguridad a las rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import TasksView from '@/views/TasksView.vue'
 import ConfigView from '@/views/ConfigView.vue'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
+import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,34 +12,65 @@ const router = createRouter({
     {
       path: '/',
       name: 'timetable',
-      component: TimetableView
+      component: TimetableView,
+      meta: {
+        Auth: true
+      }
     },
     {
       path: '/tasks',
       name: 'tasks',
-      component: TasksView
+      component: TasksView,
+      meta: {
+        Auth: true
+      }
     },
     {
       path: '/config',
       name: 'config',
-      component: ConfigView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
+      component: ConfigView,
+      meta: {
+        Auth: true
+      }
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
+      component: AboutView,
+      meta: {
+        Auth: false
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        Auth: false
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+      meta: {
+        Auth: false
+      }
     }
   ]
+})
+
+router.beforeEach(async (to, from, next) => {
+  const auth = localStorage.getItem('token') ? true : false
+  const NeedAuth = to.meta.Auth || false
+
+  if (NeedAuth && !auth) {
+    next({ name: 'login' })
+  } else if (auth && to.name?.toString() == 'login') {
+    next({ name: 'timetable' })
+  } else {
+    next()
+  }
 })
 
 export default router
