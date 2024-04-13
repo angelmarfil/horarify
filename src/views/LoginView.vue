91b2d090c68e1cfa8d47fe85521844765ff18aab
Alfonso Estrada <21393135@utcancun.edu.mx>
2024-04-13 10:57:56 -0500

    Configurando favicon y añadiendo vista de login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,63 @@
 <script setup lang="ts"></script>
 <template>
-    <div></div>
-</template>
+  <div class="flex items-start justify-center h-screen">
+    <div class="card w-full bg-base-100 m-4">
+      <figure class="px-10 pt-10">
+        <img
+          src="/favicon/android-chrome-192x192.png"
+          alt="Shoes"
+          class="rounded-xl"
+          width="100"
+          height="100"
+        />
+      </figure>
+      <form class="card-body items-center text-center gap-y-14 pt-4" method="post">
+        <h2 class="card-title">Iniciar Sesión</h2>
+        <label class="input input-bordered flex items-center gap-2 relative">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="w-4 h-4 opacity-70"
+          >
+            <path
+              d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+            />
+            <path
+              d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+            />
+          </svg>
+          <input type="text" class="grow" placeholder="Email" />
+          <span class="text-error absolute top-14">error</span>
+        </label>
 
-<style scoped></style>
+        <label class="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="w-4 h-4 opacity-70"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <input type="password" class="grow" placeholder="Contraseña" />
+        </label>
+        <div class="card-actions">
+          <button class="btn btn-primary">Ingresar</button>
+        </div>
+
+        <p class="text-sm">
+          ¿No tiene una cuenta?
+          <RouterLink :to="{ name: 'register' }" class="link link-hover text-primary"
+            >Crea una.</RouterLink
+          >
+        </p>
+        <p class="fixed bottom-10">Horarify © 2024</p>
+      </form>
+    </div>
+  </div>
+</template>
