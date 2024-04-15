9fb682fab039159c865e544f7aa01507b5fd108b
Gabriel Caamal <Carlos.chancaamal@gmail.com>
2024-04-15 00:00:05 -0500

    cambiando tipografía

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Poppins'
+    },
+    extend: {}
   },
   // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
